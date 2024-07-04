ecf6988ce6c2209c9238abd37a48a25db2b99dd1
paul <paulm@tetrardus.net>
2024-07-04 08:55:15 -0700

    more validation checks

--- C/validate_ipv4/validate_ipv4.c
+++ C/validate_ipv4/validate_ipv4.c
@@ -17,8 +17,8 @@ int validate_ipv4(char *addr) {
 	/* Validate CIDR addr length */
         len = strlen(addr);
         if (len > MAXLEN || len < MINLEN) {
-            printf("invalid length for CIDR addr\n");
-            return(-1);
+            printf("invalid addr length\n");
+            return(1);
         }
 
         /* SET VARIABLES */
@@ -29,30 +29,36 @@ int validate_ipv4(char *addr) {
         /* VALIDATE ADDR */
         for (i = 0; i < 4; i++) {
             if ((o = strsep(&addr, ".")) == NULL) {
-                printf("missing octets\n");
-                return(-1);
+		printf("missing octets\n");
+                return(1);
             }
+	 
+	    if (o[0] == '0' && strlen(o) > 1) {
+		printf("malformed octet: %s\n", o);
+		return(1);
+	    }
+	
             if ((oct = strtonum(o, 0, 255, &errstr)) == 0 && errno) {
-                printf("bad bad octet: %s\n", errstr);
-                return(-1);
+                printf("malformed octet: %s\n", errstr);
+                return(1);
             }
         }
 
         /* There should be nothing left in addr */
         if (addr != NULL) {
             printf("Too many octets\n");
-            return(-1);
+            return(1);
         }
 
 	/* VALIDATE NETMASK */
 	if (mask != NULL && *mask == '\0') {
 	    printf("Incomplete netmask\n");
-	    return(-1);
+	    return(1);
 	}
 
         if (mask != NULL && (strtonum(mask, 1, 32, &errstr) == 0) ) {
 	    printf("Netmask Error: %s\n", errstr);
-	    return(-1);
+	    return(1);
 	}
 
 	return 0;
